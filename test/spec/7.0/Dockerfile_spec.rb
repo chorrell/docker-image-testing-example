a415581496be3479e833730046fb92fb8ed00bb7
Christopher Horrell <christopher@joyent.com>
2016-11-07 21:24:57 -0500

    Add v7.0.0

---
+++ test/spec/7.0/Dockerfile_spec.rb
@@ -0,0 +1,23 @@
+require 'serverspec'
+require 'docker'
+require 'node_tests'
+require 'npm_tests'
+
+tag = ENV['TARGET_HOST']
+
+describe "#{tag}" do
+  include Helpers
+  
+  before(:all) do
+    create_image(tag)
+  end
+
+  test_node("7.0.0")
+
+  test_npm
+
+  after(:all) do
+    delete_image
+  end  
+
+end
